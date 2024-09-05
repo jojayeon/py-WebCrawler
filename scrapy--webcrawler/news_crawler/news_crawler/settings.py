49ba473d8d8687b0b5922b7189068e46390572c4
jojayeon <jojayeon6152@gmail.com>
2024-09-05 18:02:48 +0900

    차단 멘트 나오지 않게 제거

--- scrapy--webcrawler/news_crawler/news_crawler/settings.py
+++ scrapy--webcrawler/news_crawler/news_crawler/settings.py
@@ -94,3 +94,4 @@ FEED_EXPORT_ENCODING = "utf-8"
 
 # settings.py
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+ROBOTSTXT_OBEY = False
